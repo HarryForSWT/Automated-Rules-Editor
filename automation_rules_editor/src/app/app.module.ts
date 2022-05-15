3c5ebb4d4022151b82a4198634e661a8213ed156
Yiming Zhao <zym123hp@gmail.com>
2022-05-15 17:39:26 +0200

    components created

--- automation_rules_editor/src/app/app.module.ts
+++ automation_rules_editor/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NaviComponent } from './navi/navi.component';
+import { RulesComponent } from './rules/rules.component';
+import { RuleListComponent } from './rules/rule-list/rule-list.component';
+import { RuleItemComponent } from './rules/rule-list/rule-item/rule-item.component';
+import { RuleEditComponent } from './rules/rule-edit/rule-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NaviComponent,
+    RulesComponent,
+    RuleListComponent,
+    RuleItemComponent,
+    RuleEditComponent
   ],
   imports: [
     BrowserModule,
